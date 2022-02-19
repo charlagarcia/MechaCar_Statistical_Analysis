26be028453fd7621bdd6b81e4b9654cef64ff8ee
Charla Garcia <charla.a.garcia@gmail.com>
2022-02-19 17:03:19 -0600

    deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,22 @@
+## Deliverable 1 ##
+
+#load package
+library(dbplyr)
+
+#csv to df
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+head(MechaCar_mpg)
+
+#linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg)
+
+#summary stats
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
+
+
+## Deliverable 2 ##
+
+#csv to table
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
