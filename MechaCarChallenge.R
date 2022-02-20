63076ba7e51515bd7b0b4b8fb62670f2617c4bd7
Charla Garcia <charla.a.garcia@gmail.com>
2022-02-19 18:21:55 -0600

    through del 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -27,3 +27,15 @@ total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep') 
 
 ## Deliverable 3 ##
+
+#t-test for all lots
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+
+#t-test for lot 1
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
+
+#lot 2
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
+
+#lot 3
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))
