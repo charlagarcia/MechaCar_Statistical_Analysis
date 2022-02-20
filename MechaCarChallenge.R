f38ef74c5368cd5f0cb31989415244656d637761
Charla Garcia <charla.a.garcia@gmail.com>
2022-02-19 18:04:54 -0600

    through deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,7 @@
 
 #load package
 library(dbplyr)
+library(tidyverse)
 
 #csv to df
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -19,4 +20,10 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 #csv to table
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 
+#stat summary
 total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+#stat summary with group_by()
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep') 
+
+## Deliverable 3 ##
